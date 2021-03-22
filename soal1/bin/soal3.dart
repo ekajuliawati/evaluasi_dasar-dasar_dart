bcc1e2f3e73059e95af312f831bda0e68cbaeb2d
Eka Juliawati <ekajuliawati63@gmail.com>
2021-03-22 17:42:40 +0700

    soal nomor 3

---
+++ soal1/bin/soal3.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+main(List<String> args) {
+  int iGajiPokok = int.tryParse(stdin.readLineSync());
+  int iJmlAnak = int.tryParse(stdin.readLineSync());
+  double iTunjangan;
+
+  if (iJmlAnak <= 3 && iJmlAnak > 0) {
+    iTunjangan = iJmlAnak * 0.1 * iGajiPokok;
+  } else if (iJmlAnak > 3) {
+    iTunjangan = 3 * 0.1 * iGajiPokok;
+  } else {
+    iTunjangan = 0;
+  }
+  print("Tunjangan : " + iTunjangan.toString());
+}
